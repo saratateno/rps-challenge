72ea5fe14af539074d81601598ada918d117878a
Sara Tateno <sara.tateno@gmail.com>
2015-12-14 14:04:09 +0000

    Playing with CSS

--- spec/features/choose_weapons_spec.rb
+++ spec/features/choose_weapons_spec.rb
@@ -7,23 +7,20 @@ feature 'choose weapons' do
   context 'allows the player a choice of rock, paper or scissors' do
     scenario 'player chooses rock' do
       register_and_play
-      choose("rock")
-      click_button 'OK'
+      click_button("rock")
       expect(page).to have_content('You chose rock')
     end
 
     scenario 'player chooses paper' do
       register_and_play
-      choose("paper")
-      click_button 'OK'
+      click_button("paper")
       expect(page).not_to have_content('You chose rock')
       expect(page).to have_content('You chose paper')
     end
 
     scenario 'player chooses scissors' do
       register_and_play
-      choose("scissors")
-      click_button 'OK'
+      click_button("scissors")
       expect(page).not_to have_content('You chose rock')
       expect(page).to have_content('You chose scissors')
     end
@@ -33,8 +30,7 @@ feature 'choose weapons' do
     scenario 'computer chooses rock' do
       allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
       register_and_play
-      choose("rock")
-      click_button 'OK'
+      click_button("rock")
       expect(page).to have_content('The computer chose paper')
     end
   end
--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -6,22 +6,19 @@ feature 'declare the results' do
 
     scenario 'the player chose paper' do
       register_and_play
-      choose("paper")
-      click_button 'OK'
+      click_button("paper")
       expect(page).to have_content 'It\'s a draw.'
     end
 
     scenario 'the player chose rock' do
       register_and_play
-      choose("rock")
-      click_button 'OK'
+      click_button("rock")
       expect(page).to have_content 'Sorry, you lost.'
     end
 
     scenario 'the player chose scissors' do
       register_and_play
-      choose("scissors")
-      click_button 'OK'
+      click_button("scissors")
       expect(page).to have_content 'Congratulations, you won!'
     end
   end
